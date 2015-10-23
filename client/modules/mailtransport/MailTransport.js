a46668aecbc10231514b5efd09a20372b631bebf
thierry <thierry@thierry-VirtualBox>
2015-10-23 16:07:25 +0200

    - Ajout header - Amélioration prise en charge des erreurs

--- client/modules/mailtransport/MailTransport.js
+++ client/modules/mailtransport/MailTransport.js
@@ -1,3 +1,8 @@
+/* 
+ * MailTransport module
+ * (C) Androme 2015
+ * 
+ */
 
 Ext.define('MyDesktop.modules.mailtransport.MailTransport', {
     extend: 'Ext.ux.desktop.Module',
@@ -25,43 +30,83 @@ Ext.define('MyDesktop.modules.mailtransport.MailTransport', {
     },
     createWindow: function (refer) {
         var me = this;
-        var cfg = {};
         var desktop = this.app.getDesktop();
         var win = desktop.getWindow(this.id);
         if (!win) {
-            // configuration et création du store
-            var MailTransportGridStore = Ext.create('MyDesktop.modules.mailtransport.stores.MailTransport');
-            MailTransportGridStore.on({
-                update: function (store, record, operation, modifiedFieldNames, eOpts) {
-                    if (record.dirty == false)
-                        store.reload();
-                },
-                scope: this
-            });
-            MailTransportGridStore.proxy.on({
-                exception: function (proxy, response, operation) {
-                    //var operation = store.getProxy().getReader().rawData.message;
-                    var error = operation.error;
-                    var title = error.code;
-                    title += ' (' + error.errno + ') - ';
-                    title += error.sqlState;
-                    Ext.infoMsg.msg(title, error.message, 5000, 'red');
-                    if (operation.action != 'read')
-                    {
-                        Ext.data.StoreManager.get('mailtransport').reload();
+            // création et configuration du store
+            var gridStore = Ext.create('MyDesktop.modules.mailtransport.stores.MailTransport');
+            gridStore.on({
+                refresh: function (store, eOpts) {
+                    console.log('refreshdetail', store.proxy.reader.jsonData, eOpts);
+                    message = store.proxy.reader.jsonData;
+                    // on gère les messages de retour
+                    // se reporter à DOC/ZMErrorCodes
+                    switch (message.error.ZMErrorCode.toString()[0]) {
+                        case '1':
+                            console.log('add');
+                            switch (message.error.ZMErrorCode.toString()[2]) {
+                                case'0':
+                                    Ext.infoMsg.msg("Ajout d'éléments", "L'entrée a bien été ajoutée");
+                                    break;
+                            }
+                            break;
+                        case '2':
+                            console.log('destroy');
+                            switch (message.error.ZMErrorCode.toString()[2]) {
+                                case'0':
+                                    Ext.infoMsg.msg("Suppression d'éléments", "La suppression a bien été effectuée");
+                                    break;
+                                case'3':
+                                    Ext.infoMsg.msg("Suppression d'éléments", "Aucune entrée n'a été supprimée", 5000, 'orange');
+                                    store.reload();
+                                    break;
+                                case'4':
+                                    Ext.infoMsg.msg("Suppression d'éléments", "Certaines entrées n'ont pas été supprimées", 5000, 'orange');
+                                    store.reload();
+                                    break;
+                            }
+                            break;
+                        case '3':
+                            console.log('read');
+                            break;
+                        case '4':
+                            console.log('update');
+                            switch (message.error.ZMErrorCode.toString()[2]) {
+                                case'0':
+                                    Ext.infoMsg.msg("Modification d'éléments", "La modification a bien été effectuée");
+                                    break;
+                            }
+                            break;
                     }
 
                 },
                 scope: this
             });
-            // configuration et création du grid
+            gridStore.proxy.on({
+                exception: function (proxy, response, operation) {
+                    var error = operation.error;
+                    var title = error.code;
+                    title += ' (' + error.errno + ') - ';
+                    title += error.sqlState;
+                    var message = error.ZMErrorMsg;
+                    //}
+
+                    Ext.infoMsg.msg(title, message, 5000, 'red');
+                    if (operation.action != 'read')
+                    {
+                        Ext.data.StoreManager.get('mailtransport').reload();
+                    }
+                },
+                scope: this
+            });
+            // création et configuration du grid
             cfg = {
-                store: MailTransportGridStore,
+                store: gridStore,
                 rowEditing: true,
                 title: 'transport de mails',
                 multiSelect: true,
             };
-            var MailTransportGrid = Ext.create('MyDesktop.modules.common.views.PagingGrid', cfg);
+            var grid = Ext.create('MyDesktop.modules.common.views.PagingGrid', cfg);
 
             win = desktop.createWindow({
                 id: this.id,
@@ -75,7 +120,7 @@ Ext.define('MyDesktop.modules.mailtransport.MailTransport', {
                     xtype: 'tabpanel',
                     listeners: {
                         afterrender: function () {
-                            MailTransportGridStore.load();
+                            gridStore.load();
                         },
                         tabchange: function (tabPanel, newTab, oldTab, eOpts) {
                             if (newTab.store)
@@ -98,9 +143,9 @@ Ext.define('MyDesktop.modules.mailtransport.MailTransport', {
                         }
                     },
                     items: [{
-                            xtype: MailTransportGrid,
+                            xtype: grid,
                         }
-                    ]},               
+                    ]},
                 //fonctions
                 entryAdd: function (params) {
                     console.log('entryadd:', params);
