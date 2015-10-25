d5538ceed1f257cfea6cef805ac600527f497fd7
thierry <thierry@thierry-VirtualBox>
2015-10-25 14:29:44 +0100

    - mise en conformite avec modele squelette

--- direct/DXLogin.js
+++ direct/DXLogin.js
@@ -1,18 +1,11 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * DXTransport
+ * (C) Androme 2015
+ * 
  */
-var MySQLConfig = global.MySQLConfig;
 var log = global.log.child({widget_type: 'DXLogin'});
-var pool = mysql.createPool({
-    connectionLimit: 100,
-    host: MySQLConfig.host,
-    user: MySQLConfig.user,
-    password: MySQLConfig.password,
-    database: MySQLConfig.database,
-    debug: false
-});
+var pool = global.pool;
+
 
 var DXLogin = {
     // method signature has 5 parameters
@@ -31,41 +24,52 @@ var DXLogin = {
 
         pool.getConnection(function (err, connection) {
             if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
+                err.ZMTypeCode = 'LOGIN';
+                err.ZMErrorCode = 101;
+                sendError(err, callback);
             }
-            connection.escape(params.username);
-            connection.escape(params.password);
-            //console.log('connected as id ' + connection.threadId);
-            var query = "SELECT * from users WHERE username='" + params.username + "'";
-            query += " AND password='" + params.password + "'";
+            else
+            {
+                setLanguage(connection, request);
+                connection.escape(params.username);
+                connection.escape(params.password);
+                var query = "SELECT * from users WHERE username='" + params.username + "'";
+                query += " AND password='" + params.password + "'";
+                connection.query(query, function (err, rows, fields) {
+                    if (!err) {
+                        if (rows.length !== 0) {
+                            success = true;
+                            message = 'Login successful';
+                            data.userinfo = rows[0];
+                            request.session.userinfo = data.userinfo;
+                            log.info('Login of ' + request.session.userinfo.username);
+                            var message = {
+                                'ZMTypeCode': 'LOGIN',
+                                'ZMErrorCode': 100
+                            }
+                            sendSuccess(rows.length, data, callback, message);
 
-            connection.query(query, function (err, rows) {
-                connection.release();
-                if (!err) {
-                    if (rows.length !== 0) {
-                        success = true;
-                        message = 'Login successful';
-                        data.userinfo = rows[0];
-                        request.session.userinfo = data.userinfo;
-                        //console.log(request.session.userinfo);
-                        log.info('Login of ' + request.session.userinfo.username);
+                        } else
+                        {
+                            var message = {
+                                'ZMTypeCode': 'LOGIN',
+                                'ZMErrorCode': 103
+                            }
+                            sendSuccess(rows.length, null, callback, message);
+                        }
                     }
-
-                    callback({
-                        success: success,
-                        message: message,
-                        data: data
-                    });
-                }
-            });
-
-            connection.on('error', function (err) {
-                log.warn("Error connecting database ... \n\n");
-                return;
-            });
+                    else
+                    {
+                        err.ZMTypeCode = 'LOGIN';
+                        err.ZMErrorCode = 102;
+                        err.ZMErrorMsg = String(err);
+                        console.log('erreurlogin', err);
+                        sendError(err, callback);
+                    }
+                });
+            }
+            if (connection)
+                connection.release();
         });
     },
     getsession: function (params, callback, sessionID, request, response) {
@@ -78,34 +82,87 @@ var DXLogin = {
             // on ne fournit que les info utilisateur et pas les autres
             // comme les infos coockies
             data.userinfo = request.session.userinfo;
-
+            var message = {
+                'ZMTypeCode': 'LOGIN',
+                'ZMErrorCode': 300
+            }
+            sendSuccess(null, data, callback, message);
         }
-        callback({
-            success: success,
-            data: data
-        });
+        else
+        {
+            err.ZMTypeCode = 'LOGIN';
+            err.ZMErrorCode = 303;
+            err.ZMErrorMsg = null;
+            sendError(err, callback);
+        }
     },
     isvalidsession: function (params, callback, sessionID, request, response) {
         response.header('My-Custom-Header ', '1234567890');
         var success = false;
         if (request.session.userinfo) {
             success = true;
+            var message = {
+                'ZMTypeCode': 'LOGIN',
+                'ZMErrorCode': 300
+            }
+            sendSuccess(null, data, callback, message);
         }
-        callback({
-            success: success,
-        });
+        else
+        {
+            err.ZMTypeCode = 'LOGIN';
+            err.ZMErrorCode = 303;
+            err.ZMErrorMsg = null;
+            sendError(err, callback);
+        }
+        /*callback({
+         success: success,
+         });*/
     },
     logout: function (params, callback, sessionID, request, response) {
-        var data;
+        //var data;
         response.header('My-Custom-Header ', '1234567890');
-        request.session.userinfo = null;
-        delete request.session.userinfo;
-        callback({
-            success: true,
-            message: 'logout',
-            data: data
-        });
+        //request.session.userinfo = null;
+        if (request.session.userinfo)
+            delete request.session.userinfo;
+        var message = {
+            'ZMTypeCode': 'LOGIN',
+            'ZMErrorCode': 102
+        }
+        sendSuccess(null, null, callback, message);
     }
 };
+
+function sendError(err, callback) {
+    log.error(err);
+    callback({
+        success: false,
+        error: err,
+    });
+}
+
+function sendSuccess(totalCount, data, callback, message) {
+    var msg = '';
+    if (message)
+        msg = message;
+    callback({
+        success: true,
+        totalCount: totalCount,
+        error: msg,
+        data: data
+    });
+}
+function setLanguage(connection, request)
+{
+    var lang = "fr_FR";
+    if (request.session.userinfo) {
+        if (request.session.userinfo.lang) {
+            lang = request.session.userinfo.lang;
+        }
+    }
+    var query = "SET lc_messages = '" + lang + "'";
+    connection.query(query, function (err, rows, fields) {
+    });
+}
+
 
 module.exports = DXLogin;
