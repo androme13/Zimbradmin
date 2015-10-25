61f13b39158232267011788e2fec762859eceb88
thierry <thierry@thierry-VirtualBox>
2015-10-25 19:16:26 +0100

    - Sortie des fonction communes à tous les DX dans un fichier DXCommun (plus facile pour la maintenance et l'évolution)

--- direct/DXTransport.js
+++ direct/DXTransport.js
@@ -5,6 +5,7 @@
  */
 var log = global.log.child({widget_type: 'DXTransport'});
 var pool = global.pool;
+var DXCommon = require('../tools/DXCommon.js');
 var DXTransport = {
     // method signature has 5 parameters
     /**
@@ -24,11 +25,11 @@ var DXTransport = {
                 err.ZMTypeCode = 'DX';
                 err.ZMErrorCode = 102;
                 err.ZMErrorMsg = String(err);
-                sendError(err, callback);
+                DXCommon.sendError(err, callback);
             }
             else
             {
-                setLanguage(connection, request);
+                DXCommon.setLanguage(connection, request);
                 var myId = request.session.userinfo.id;
                 query = "INSERT INTO transport (domain, transport, created_by) VALUES ('" + params[0].domain.toLowerCase() + "','" + params[0].transport.toLowerCase() + "','" + myId + "')";
                 connection.query(query, function (err, rows, fields) {
@@ -37,14 +38,14 @@ var DXTransport = {
                             'ZMTypeCode': 'DX',
                             'ZMErrorCode': 100
                         }
-                        sendSuccess(rows.length, rows, callback, message);
+                        DXCommon.sendSuccess(rows.length, rows, callback, message);
                     }
                     else
                     {
                         err.ZMTypeCode = 'DX';
                         err.ZMErrorCode = 102;
                         err.ZMErrorMsg = String(err);
-                        sendError(err, callback);
+                        DXCommon.sendError(err, callback);
                     }
                 });
             }
@@ -60,11 +61,11 @@ var DXTransport = {
                 err.ZMTypeCode = 'DX';
                 err.ZMErrorCode = 202;
                 err.ZMErrorMsg = String(err);
-                sendError(err, callback);
+                DXCommon.sendError(err, callback);
             }
             else
             {
-                setLanguage(connection, request);
+                DXCommon.setLanguage(connection, request);
                 var id = request.session.userinfo.id;
                 var occur = '';
                 var temp = '';
@@ -109,14 +110,14 @@ var DXTransport = {
                                 ZMErrorCode: 203,
                             };
                         }
-                        sendSuccess(rows.length, rows, callback, message);
+                        DXCommon.sendSuccess(rows.length, rows, callback, message);
                     }
                     else
                     {
                         err.ZMTypeCode = 'DX';
                         err.ZMErrorCode = 202;
                         err.ZMErrorMsg = String(err);
-                        sendError(err, callback);
+                        DXCommon.sendError(err, callback);
                     }
                 });
             }
@@ -144,11 +145,11 @@ var DXTransport = {
                 err.ZMTypeCode = 'DX';
                 err.ZMErrorCode = 302;
                 err.ZMErrorMsg = String(err)
-                sendError(err, callback);
+                DXCommon.sendError(err, callback);
             }
             else
             {
-                setLanguage(connection, request);
+                DXCommon.setLanguage(connection, request);
                 query = "SELECT * FROM transport " + extraQuery + " LIMIT " + params.start + ',' + params.limit;
                 connection.query(query, function (err, rows, fields) {
                     if (!err) {
@@ -162,14 +163,14 @@ var DXTransport = {
                                     'ZMTypeCode': 'DX',
                                     'ZMErrorCode': 300
                                 };
-                                sendSuccess(rows[0].totalCount, data, callback, message);
+                                DXCommon.sendSuccess(rows[0].totalCount, data, callback, message);
                             }
                             else
                             {
                                 err.ZMTypeCode = 'DX';
                                 err.ZMErrorCode = 302;
                                 err.ZMErrorMsg = String(err)
-                                sendError(err, callback);
+                                DXCommon.sendError(err, callback);
                             }
                         })
                     }
@@ -178,7 +179,7 @@ var DXTransport = {
                         err.ZMTypeCode = 'DX';
                         err.ZMErrorCode = 302;
                         err.ZMErrorMsg = String(err)
-                        sendError(err, callback);
+                        DXCommon.sendError(err, callback);
                     }
                 });
             }
@@ -192,11 +193,11 @@ var DXTransport = {
             if (err) {
                 err.ZMTypeCode = 'DX';
                 err.ZMErrorCode = 402;
-                sendError(err, callback);
+                DXCommon.sendError(err, callback);
             }
             else
             {
-                setLanguage(connection, request);
+                DXCommon.setLanguage(connection, request);
                 var myId = request.session.userinfo.id;
                 query = "UPDATE transport SET domain ='" + params[0].domain.toLowerCase();
                 query += "', transport='" + params[0].transport.toLowerCase();
@@ -209,7 +210,7 @@ var DXTransport = {
                             'ZMTypeCode': 'DX',
                             'ZMErrorCode': 400
                         }
-                        sendSuccess(rows.length, rows, callback, message);
+                        DXCommon.sendSuccess(rows.length, rows, callback, message);
                     }
                     else
                     {
@@ -217,7 +218,7 @@ var DXTransport = {
                         err.ZMErrorCode = 402;
                         err.ZMErrorMsg = String(err);
                         //console.log('erreur', err);
-                        sendError(err, callback);
+                        DXCommon.sendError(err, callback);
                     }
                 });
             }
@@ -226,37 +227,5 @@ var DXTransport = {
         });
     }
 };
-
-function sendError(err, callback) {
-    log.error(err);
-    callback({
-        success: false,
-        error: err,
-    });
-}
-
-function sendSuccess(totalCount, data, callback, message) {
-    var msg = '';
-    if (message)
-        msg = message;
-    callback({
-        success: true,
-        totalCount: totalCount,
-        error: msg,
-        data: data
-    });
-}
-function setLanguage(connection, request)
-{
-    var lang = "fr_FR";
-    if (request.session.userinfo) {
-        if (request.session.userinfo.lang) {
-            lang = request.session.userinfo.lang;
-        }
-    }
-    var query = "SET lc_messages = '" + lang + "'";
-    connection.query(query, function (err, rows, fields) {
-    });
-}
 
 module.exports = DXTransport;
