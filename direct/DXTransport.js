d452d595e25f4ebbd329100ac1f1dfef7ec5af57
thierry <thierry@thierry-VirtualBox>
2015-10-26 17:44:45 +0100

    -sortie de la fonction destroy et add

--- direct/DXTransport.js
+++ direct/DXTransport.js
@@ -17,122 +17,52 @@ var DXTransport = {
      * @param response only if "appendRequestResponseObjects" enabled
      */
 
-
     add: function (params, callback, sessionID, request, response) {
-        var query;
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                err.ZMTypeCode = 'DX';
-                err.ZMErrorCode = 102;
-                err.ZMErrorMsg = String(err);
-                DXCommon.sendError(err, callback);
-            }
-            else
-            {
-                DXCommon.setLanguage(connection, request);
-                var myId = request.session.userinfo.id;
-                DXCommon.setLanguage(connection, request);
-                query = "INSERT INTO transport (domain, transport, created_by) VALUES ('" + params[0].domain.toLowerCase() + "','" + params[0].transport.toLowerCase() + "','" + myId + "')";
-                connection.query(query, function (err, rows, fields) {
-                    if (!err) {
-                        var message = {
-                            'ZMTypeCode': 'DX',
-                            'ZMErrorCode': 100
-                        }
-                        DXCommon.sendSuccess(rows.length, rows, callback, message);
-                    }
-                    else
-                    {
-                        err.ZMTypeCode = 'DX';
-                        err.ZMErrorCode = 102;
-                        err.ZMErrorMsg = String(err);
-                        DXCommon.sendError(err, callback);
-                    }
-                });
-            }
-            if (connection)
-                connection.release();
-        });
-
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        params[0].table = 'transport';
+        params[0].log = log;
+        var myId = request.session.userinfo.id;
+        query = "INSERT INTO " + params[0].table + " (domain, transport, created_by) VALUES ('" + params[0].domain.toLowerCase() + "','" + params[0].transport.toLowerCase() + "','" + myId + "')";
+        params[0].query = query;
+        DXCommon.add(params[0], callback, sessionID, request, response);
     },
     destroy: function (params, callback, sessionID, request, response) {
-        var query;
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                err.ZMTypeCode = 'DX';
-                err.ZMErrorCode = 202;
-                err.ZMErrorMsg = String(err);
-                DXCommon.sendError(err, callback);
-            }
-            else
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        var newParams = {};
+        newParams.table = 'transport';
+        newParams.log = log;
+        newParams.length = params.length;
+        var occur = '';
+        var temp = '';
+        var count = 0;
+        params.forEach(function (entry) {
+            count++;
+            // test erreur///
+            //if (count == 2)
+            // entry.domain = 'aa' + entry.domain;
+            temp = "(" + entry.id + ",'" + entry.domain + "','" + entry.transport + "')";
+            if (count < params.length)
             {
-                DXCommon.setLanguage(connection, request);
-                var id = request.session.userinfo.id;
-                var occur = '';
-                var temp = '';
-                var count = 0;
-                params.forEach(function (entry) {
-                    count++;
-                    // test erreur///
-                    //if (count == 2)
-                    // entry.domain = 'aa' + entry.domain;
-                    temp = "(" + entry.id + ",'" + entry.domain + "','" + entry.transport + "')";
-                    if (count < params.length)
-                    {
-                        temp += ',';
-                    }
-                    occur += temp;
-                });
-                query = "DELETE FROM transport WHERE (id,domain,transport) IN (" + occur + ")";
-                connection.query(query, function (err, rows, fields) {
-                    if (!err) {
-                        // si toutes les entrées ont été supprimées
-                        if (params.length == rows.affectedRows)
-                        {
-                            var message = {
-                                ZMTypeCode: 'DX',
-                                ZMErrorCode: 200
-                            }
-                        }
-                        // si seulement certaines entrées ont ét suprimées
-                        if (params.length > rows.affectedRows)
-                        {
-                            //console.log(err, rows);
-                            var message = {
-                                ZMTypeCode: 'DX',
-                                ZMErrorCode: 204,
-                            };
-                        }
-                        // si aucune entrée n'a été supprimée
-                        if (rows.affectedRows == 0)
-                        {
-                            var message = {
-                                ZMTypeCode: 'DX',
-                                ZMErrorCode: 203,
-                            };
-                        }
-                        DXCommon.sendSuccess(rows.length, rows, callback, message);
-                    }
-                    else
-                    {
-                        err.ZMTypeCode = 'DX';
-                        err.ZMErrorCode = 202;
-                        err.ZMErrorMsg = String(err);
-                        DXCommon.sendError(err, callback);
-                    }
-                });
+                temp += ',';
             }
-            if (connection)
-                connection.release();
+            occur += temp;
         });
-
+        query = "DELETE FROM " + newParams.table + " WHERE (id,domain,transport) IN (" + occur + ")";
+        newParams.query = query;
+        DXCommon.destroy(newParams, callback, sessionID, request, response);
     },
     get: function (params, callback, sessionID, request, response) {
         var query, extraQuery;
         // on set les parametres par défaut si ils sont absents
         if (!params)
             var params = {};
-        params.extraQuery='';
+        params.extraQuery = '';
         params.table = 'transport';
         if (!params.col)
             params.col = 'domain';
@@ -157,11 +87,11 @@ var DXTransport = {
         // on set les parametres par défaut si ils sont absents
         if (!params) {
             var params = [];
-            params[0]={};
+            params[0] = {};
         }
         params[0].table = 'transport';
         params[0].log = log;
-        query = "UPDATE " + params[0].table + " SET domain='" +params[0].domain.toLowerCase();
+        query = "UPDATE " + params[0].table + " SET domain='" + params[0].domain.toLowerCase();
         query += "', transport='" + params[0].transport.toLowerCase();
         query += "', modified_by='" + myId;
         query += "' WHERE id='" + params[0].id + "'";
