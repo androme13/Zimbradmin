b0ed65255ebfc77e9736c6b39dc76d1e96552692
thierry <thierry@thierry-VirtualBox>
2015-10-27 20:22:25 +0100

    - corrections d'un bug dans la recherche

--- direct/DXSmtp.js
+++ direct/DXSmtp.js
@@ -1,19 +1,11 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- *///
-var fs = require('fs');
-var MySQLConfig = global.MySQLConfig;
-var log = global.log.child({widget_type: 'DXSmtp'});
-var pool = mysql.createPool({
-    connectionLimit: 100,
-    host: MySQLConfig.host,
-    user: MySQLConfig.user,
-    password: MySQLConfig.password,
-    database: MySQLConfig.database,
-    debug: false
-});
+ * DXSmtp
+ * (C) Androme 2015
+ * 
+ */
+var log = global.log.child({widget_type: 'DXUser'});
+var pool = global.pool;
+var DXCommon = require('../tools/DXCommon.js');
 
 var DXSmtp = {
     // method signature has 5 parameters
@@ -27,127 +19,100 @@ var DXSmtp = {
      */
 
 
-    // operations sur les user ////////////////////////////////
-    addusers: function (params, callback, sessionID, request, response) {
-        pool.getConnection(function (err, connection) {
-            console.log(params);
-            if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            }
-            var query = "SELECT id,level,state,username,firstname,lastname,created_date,created_by,modified_date,modified_by FROM users "
-
-            connection.query(query, function (err, rows) {
-                connection.release();
-                if (!err) {
-                    if (rows.length !== 0) {
-                        success = true;
-                        data = rows;
-                    }
-                    callback({
-                        success: false,
-                        message: "addusers",
-                        data: data
-                    });
-                }
-            });
-
-            connection.on('error', function (err) {
-                //res.json({"code" : 100, "status" : "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            });
-        });
+    // operations sur les serveur smtp ////////////////////////////////
+    add: function (params, callback, sessionID, request, response) {
+        // mono requete, à voir plus tard pour du multi-requete
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        params[0].table = 'smtp_servers';
+        params[0].log = log;
+        var myId = request.session.userinfo.id;
+        var query = "INSERT INTO " + params[0].table;
+        query += " (state,server,comment,created_by) VALUES ('";
+        query += params[0].state + "','";
+        query += params[0].server.toLowerCase() + "','";
+        query += params[0].comment.toLowerCase() + "','";
+        query += myId + "')";
+        //var query = "SELECT id,level,state,username,firstname,lastname,created_date,created_by,modified_date,modified_by FROM ";
+        params[0].query = query;
+        DXCommon.add(params[0], callback, sessionID, request, response);
     },
-    destroyusers: function (params, callback, sessionID, request, response) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
+    destroy: function (params, callback, sessionID, request, response) {
+        // multi requete
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        var newParams = {};
+        newParams.table = 'smtp_servers';
+        newParams.log = log;
+        newParams.length = params.length;
+        var occur = '';
+        var temp = '';
+        var count = 0;
+        params.forEach(function (entry) {
+            count++;
+            // test erreur///
+            //if (count == 2)
+            // entry.domain = 'aa' + entry.domain;
+            temp = "(" + entry.id + ",";
+            temp += entry.state + ",'";
+            temp += entry.server + "','";
+            temp += entry.comment + "')";
+            if (count < params.length)
+            {
+                temp += ',';
             }
-            console.log(params);
-            var query = "SELECT id,level,state,username,firstname,lastname,created_date,created_by,modified_date,modified_by FROM users "
-
-            connection.query(query, function (err, rows) {
-                connection.release();
-                if (!err) {
-                    if (rows.length !== 0) {
-                        success = true;
-                        data = rows;
-                    }
-                    callback({
-                        success: false,
-                        message: "getuserslist",
-                        data: data
-                    });
-                }
-            });
-
-            connection.on('error', function (err) {
-                //res.json({"code" : 100, "status" : "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            });
+            occur += temp;
         });
+        var query = "DELETE FROM " + newParams.table + " WHERE (id,state,server,comment) IN (" + occur + ")";
+        newParams.query = query;
+        DXCommon.destroy(newParams, callback, sessionID, request, response);
     },
     get: function (params, callback, sessionID, request, response) {
-        var query,extraQuery;
-        if (params.search) extraQuery="WHERE domain LIKE '%"+params.search+"%'";
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            }
-            query = "SELECT * FROM transport "+extraQuery+" LIMIT " + params.start + ',' + params.limit;
-            connection.query(query, function (err, rows, fields) {
-                if (!err) {
-                    if (rows.length !== 0) {
-                        success = true;
-                        data = rows;
-                        // on cherche maintenant le nombre total
-                        // d'entrées dans la table pour le paging
-                        query = "SELECT COUNT(*) AS totalCount FROM transport "+extraQuery;
-                        connection.query(query, function (err, rows, fields) {
-                            connection.release();
-                            //console.log(fields);
-                            if (!err) {
-                                //console.log('totalcount', rows[0].totalCount);
-                            }
-                            callback({
-                                success: success,
-                                totalCount: rows[0].totalCount,
-                                message: "getsmtp",
-                                data: data
-                            });
-                        })
-                    }
-                }
-            });
-
-            connection.on('error', function (err) {
-                //res.json({"code" : 100, "status" : "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            });
-        });
+        var query, extraQuery;
+        // on set les parametres par défaut si ils sont absents
+        if (!params)
+            var params = {};
+        if (!params.extraQuery)
+            params.extraQuery = '';
+        params.table = 'smtp_servers';
+        if (!params.col)
+            params.col = 'server';
+        if (!params.start)
+            params.start = 0;
+        if (!params.limit)
+            params.limit = 50;
+        if (params.search) {
+            params.extraQuery = " WHERE " + params.col;
+            params.extraQuery += " LIKE '%" + params.search + "%'";
+        }
+        params.log = log;
+        var query = "SELECT * FROM " + params.table + params.extraQuery;
+        query += " LIMIT " + params.start + ',' + params.limit;
+        params.query = query;
+        DXCommon.get(params, callback, sessionID, request, response);
     },
-    updateusers: function (params, callback, sessionID, request, response) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            }
-            console.log(params);
-            //log.infos(params);
-        });
+    update: function (params, callback, sessionID, request, response) {
+        // mono requete, à voir plus tard pour du multi-requete
+        var query;
+        var myId = request.session.userinfo.id;
+        // on set les parametres par défaut si ils sont absents
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        params[0].table = 'smtp_servers';
+        params[0].log = log;
+        query = "UPDATE " + params[0].table + " SET state ='" + params[0].state;
+        query += "', server ='" + params[0].server;
+        query += "', comment ='" + params[0].comment;
+        query += "', modified_by='" + myId;
+        query += "' WHERE id ='" + params[0].id + "'";
+        params[0].query = query;
+        DXCommon.update(params, callback, sessionID, request, response);
     },
 };
 
