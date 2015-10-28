ee00ffb1380410ddbe3708cb31a00a3d69c1b7c3
thierry <thierry@thierry-VirtualBox>
2015-10-28 19:10:09 +0100

    - application du modele squelette

--- direct/DXModules.js
+++ direct/DXModules.js
@@ -1,19 +1,11 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * DXModules
+ * (C) Androme 2015
+ * 
  */
-var fs = require('fs');
-var MySQLConfig = global.MySQLConfig;
 var log = global.log.child({widget_type: 'DXModules'});
-var pool = mysql.createPool({
-    connectionLimit: 100,
-    host: MySQLConfig.host,
-    user: MySQLConfig.user,
-    password: MySQLConfig.password,
-    database: MySQLConfig.database,
-    debug: false
-});
+var pool = global.pool;
+var DXCommon = require('../tools/DXCommon.js');
 
 var DXModules = {
     // method signature has 5 parameters
@@ -25,76 +17,92 @@ var DXModules = {
      * @param request only if "appendRequestResponseObjects" enabled
      * @param response only if "appendRequestResponseObjects" enabled
      */
-
-    addmodules: function (params, callback, sessionID, request, response) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            }
-            //console.log('connected as id ' + connection.threadId);
-            var query = "SELECT * FROM modules "
-
-
-            connection.query(query, function (err, rows) {
-                connection.release();
-                if (!err) {
-                    if (rows.length !== 0) {
-                        success = true;
-                        data = rows;
-                    }
-
-                    callback({
-                        success: success,
-                        message: "getmodules",
-                        data: data
-                    });
-                }
-            });
-
-            connection.on('error', function (err) {
-                //res.json({"code" : 100, "status" : "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            });
-        });
+    add: function (params, callback, sessionID, request, response) {
+        // mono requete, à voir plus tard pour du multi-requete
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        params[0].table = 'modules';
+        params[0].log = log;
+        var myId = request.session.userinfo.id;
+        var query = "INSERT INTO " + params[0].table;
+        query += " (module, comment, created_by) VALUES ('";
+        query += params[0].domain.toLowerCase() + "','";
+        query += params[0].transport.toLowerCase() + "','" + myId + "')";
+        params[0].query = query;
+        DXCommon.add(params[0], callback, sessionID, request, response);
     },
-    getmodules: function (params, callback, sessionID, request, response) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                //res.json({"code": 100, "status": "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
+    destroy: function (params, callback, sessionID, request, response) {
+        // multi requete
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        var newParams = {};
+        newParams.table = 'modules';
+        newParams.log = log;
+        newParams.length = params.length;
+        var occur = '';
+        var temp = '';
+        var count = 0;
+        params.forEach(function (entry) {
+            count++;
+            // test erreur///
+            //if (count == 2)
+            // entry.domain = 'aa' + entry.domain;
+            temp = "(" + entry.id + ",'";
+            temp += entry.domain + "','";
+            temp += entry.transport + "')";
+            if (count < params.length)
+            {
+                temp += ',';
             }
-            //console.log('connected as id ' + connection.threadId);
-            var query = "SELECT * FROM modules "
-
-
-            connection.query(query, function (err, rows) {
-                connection.release();
-                if (!err) {
-                    if (rows.length !== 0) {
-                        success = true;
-                        data = rows;
-                    }
-
-                    callback({
-                        success: success,
-                        message: "getmodules",
-                        data: data
-                    });
-                }
-            });
-
-            connection.on('error', function (err) {
-                //res.json({"code" : 100, "status" : "Error in connection database"});
-                log.warn("Error connecting database ... \n\n");
-                return;
-            });
+            occur += temp;
         });
+        var query = "DELETE FROM " + newParams.table + " WHERE (id,module,comment) IN (" + occur + ")";
+        newParams.query = query;
+        DXCommon.destroy(newParams, callback, sessionID, request, response);
+    },
+    get: function (params, callback, sessionID, request, response) {
+        // on set les parametres par défaut si ils sont absents
+        if (!params)
+            var params = {};
+        if (!params.extraQuery)
+            params.extraQuery = '';
+        params.table = 'modules';
+        if (!params.col)
+            params.col = 'module';
+        if (!params.start)
+            params.start = 0;
+        if (!params.limit)
+            params.limit = 50;
+        if (params.search) {
+            params.extraQuery = " WHERE " + params.col;
+            params.extraQuery += " LIKE '%" + params.search + "%'";
+        }
+        params.log = log;
+        var query = "SELECT * FROM " + params.table + params.extraQuery;
+        query += " LIMIT " + params.start + ',' + params.limit;
+        params.query = query;
+        DXCommon.get(params, callback, sessionID, request, response);
+    },
+    update: function (params, callback, sessionID, request, response) {
+        // mono requete, à voir plus tard pour du multi-requete
+        var myId = request.session.userinfo.id;
+        // on set les parametres par défaut si ils sont absents
+        if (!params) {
+            var params = [];
+            params[0] = {};
+        }
+        params[0].table = 'modules';
+        params[0].log = log;
+        var query = "UPDATE " + params[0].table + " SET module='" + params[0].module;
+        query += "', comment='" + params[0].comment;
+        query += "', modified_by='" + myId;
+        query += "' WHERE id='" + params[0].id + "'";
+        params[0].query = query;
+        DXCommon.update(params, callback, sessionID, request, response);
     },
     addmodulesshortcut: function (params, callback, sessionID, request, response) {
         pool.getConnection(function (err, connection) {
@@ -104,10 +112,6 @@ var DXModules = {
                 log.warn("Error connecting database ... \n\n");
                 return;
             }
-            //console.log('connected as id ' + connection.threadId);
-            //var query = "SELECT * FROM modules ";
-            //var query = 'UPDATE modules SET hasshortcut=1 WHERE id=1';
-
             var query = "UPDATE usersmodules " +
                     "INNER JOIN users " +
                     "ON users.id=usersmodules.userid " +
@@ -115,14 +119,6 @@ var DXModules = {
                     "ON (modules.id=usersmodules.moduleid AND modules.module='" + params.module + "') " +
                     "SET usersmodules.hasshortcut=1 " +
                     "WHERE usersmodules.userid=" + params.id;
-
-            /*var query = "SELECT userid,moduleid,hasshortcut FROM usersmodules " +
-             "INNER JOIN users " +
-             "ON users.id=usersmodules.userid " +
-             "INNER JOIN modules " +
-             "ON (modules.id=usersmodules.moduleid AND modules.module='" + params.module + "') " +
-             "WHERE usersmodules.userid=" + params.id;*/
-
 
             connection.query(query, function (err, rows) {
                 connection.release();
@@ -155,10 +151,6 @@ var DXModules = {
                 log.warn("Error connecting database ... \n\n");
                 return;
             }
-            //console.log('connected as id ' + connection.threadId);
-            //var query = "SELECT * FROM modules ";
-            //var query = 'UPDATE modules SET hasshortcut=1 WHERE id=1';
-
             var query = "UPDATE usersmodules " +
                     "INNER JOIN users " +
                     "ON users.id=usersmodules.userid " +
@@ -166,14 +158,6 @@ var DXModules = {
                     "ON (modules.id=usersmodules.moduleid AND modules.module='" + params.module + "') " +
                     "SET usersmodules.hasshortcut=0 " +
                     "WHERE usersmodules.userid=" + params.id;
-
-            /*var query = "SELECT userid,moduleid,hasshortcut FROM usersmodules " +
-             "INNER JOIN users " +
-             "ON users.id=usersmodules.userid " +
-             "INNER JOIN modules " +
-             "ON (modules.id=usersmodules.moduleid AND modules.module='" + params.module + "') " +
-             "WHERE usersmodules.userid=" + params.id;*/
-
 
             connection.query(query, function (err, rows) {
                 connection.release();
@@ -199,5 +183,4 @@ var DXModules = {
         });
     }
 };
-
 module.exports = DXModules;
