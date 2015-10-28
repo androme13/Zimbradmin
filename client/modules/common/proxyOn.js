5df967d5a3ae46a305a2ee6a59601c4dafaf2d6c
thierry <thierry@thierry-VirtualBox>
2015-10-28 18:26:03 +0100

    - introduction fonctions communes génériques aux modules

---
+++ client/modules/common/proxyOn.js
@@ -0,0 +1,28 @@
+/* 
+ * gridStore
+ * (C) Androme 2015
+ * 
+ */
+// fonction générique communes aux modules
+
+Ext.define('MyDesktop.modules.common.proxyOn', {
+    create: function (storeName) {
+        var listener = {
+            exception: function (proxy, response, operation) {
+                var error = operation.error;
+                var title = error.code;
+                title += ' (' + error.errno + ') - ';
+                title += error.sqlState;
+                var message = error.ZMErrorMsg;
+                Ext.infoMsg.msg(title, message, 5000, 'red');
+                if (operation.action !== 'read')
+                {
+                    storeName.reload();
+                }
+            },
+            scope: this
+        };
+        return listener;
+    }
+});
+
