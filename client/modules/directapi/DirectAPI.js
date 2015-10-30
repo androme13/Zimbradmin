f440830261673a27dcc32cbc83e04fd122e4f955
thierry <thierry@thierry-VirtualBox>
2015-10-30 13:30:56 +0100

    BUGFIX :  correction du port affiché lors du lancement

--- client/modules/directapi/DirectAPI.js
+++ client/modules/directapi/DirectAPI.js
@@ -14,7 +14,7 @@ Ext.define('MyDesktop.modules.directapi.DirectAPI', {
         wasLoading = Loader.isLoading;
 
     //Loading API
-    Loader.loadScriptFile('https://10.0.0.19:8080/directapi', Ext.emptyFn, Ext.emptyFn, null, true);
+    Loader.loadScriptFile('https://10.0.0.19/directapi', Ext.emptyFn, Ext.emptyFn, null, true);
     Loader.isLoading = wasLoading;
     /*
      Add provider. Name must match settings on serverside
