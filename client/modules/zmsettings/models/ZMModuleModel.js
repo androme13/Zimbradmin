228246723cf86e64ccf1c965a1d9d841a28ca448
thierry <thierry@thierry-VirtualBox>
2015-11-20 15:41:37 +0100

    - utilisation des fonctions génériques (gridCols) pour la création de certaines colonnes

--- client/modules/zmsettings/models/ZMModuleModel.js
+++ client/modules/zmsettings/models/ZMModuleModel.js
@@ -7,82 +7,9 @@
 Ext.define('MyDesktop.modules.zmsettings.models.ZMModuleModel', {
     extend: 'Ext.data.Model',
     fields: [
-        {
-            name: 'id',
-            type: 'int',
-            hidden: true,
-            flex: 1
-        },
-        {
-            name: 'state',
-            type: 'int',
-            flex: 1,
-            editor: {
-                xtype: 'combo',
-                valueField: 'value',
-                queryMode: 'local',
-                store: [[0, 'Inactif'], [1, 'Bloqué'], [2, 'Actif']],
-                editable: false,
-            },
-            renderer: function (value, meta) {
-                switch (value) {
-                    case 0 :
-                        meta.css = 'zmsettings-disabled_entry24';
-                        meta.tdAttr = 'data-qtip="Inactif"';
-                        break;
-                    case 1 :
-                        meta.css = 'zmsettings-enabled_entry24';
-                        meta.tdAttr = 'data-qtip="Actif"';
-                        break;
-                }
-                return '';
-            },
-        },
-        {
-            name: 'level',
-            text: 'Niveau',
-            defaultValue: 1,
-            flex: 2,
-            editor: {
-                xtype: 'combo',
-                editable: false,
-                //typeAhead: true,
-                triggerAction: 'all',
-                store: [
-                    [0, 'invité'],
-                    [1, 'utilisateur'],
-                    [2, 'super utilisateur'],
-                    [3, 'administrateur'],
-                    [4, 'super administrateur']
-                ]
-
-            },
-            renderer: function (value) {
-                var backval = "";
-                switch (value)
-                {
-                    case 0 :
-                        backval = "invité";
-                        break;
-                    case 1 :
-                        backval = "utilisateur";
-                        break;
-                    case 2 :
-                        backval = "super utilisateur";
-                        break;
-                    case 3 :
-                        backval = "administrateur";
-                        break;
-                    case 4 :
-                        backval = "super administrateur";
-                        break;
-                    default:
-                        backval = "inconnu";
-                        break;
-                }
-                return backval;
-            }
-        },
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createIdCol(),
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createStateCol(),
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createLevelCol(),
         {
             name: 'module',
             flex: 2,
@@ -91,14 +18,7 @@ Ext.define('MyDesktop.modules.zmsettings.models.ZMModuleModel', {
             },
             type: 'string'
         },
-        {
-            name: 'comment',
-            flex: 2,
-            editor: {
-                allowBlank: false,
-            },
-            type: 'string'
-        }
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createCommentCol(),
     ]
 });
 
