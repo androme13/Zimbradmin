06e6b9cb069523b177ccb8a6f068d4773fb73382
thierry <thierry@thierry-VirtualBox>
2015-11-20 15:46:27 +0100

    - utilisation des fonctions génériques (gridCols) pour la création de certaines colonnes

--- client/modules/router/models/MyNetworkModel.js
+++ client/modules/router/models/MyNetworkModel.js
@@ -8,32 +8,8 @@ Ext.define('MyDesktop.modules.router.models.MyNetworkModel', {
     extend: 'Ext.data.Model',
     fields: [
         // IMPORTANT : le champ id ne doit pas avoir de valeur par défaut
-        {name: 'id', hidden: true},
-        {name: 'state',
-            text: 'Etat',
-            type: 'int',
-            defaultValue: 1,
-            editor: {
-                xtype: 'combo',
-                valueField: 'value',
-                queryMode: 'local',
-                store: [[0, 'Inactif'], [1, 'Actif']],
-                editable: false,
-            },
-            renderer: function (value, meta) {
-                switch (value) {
-                    case 0 :
-                        meta.css = 'router-disabled_entry24';
-                        meta.tdAttr = 'data-qtip="Inactif"';
-                        break;
-                    case 1 :
-                        meta.css = 'router-enabled_entry24';
-                        meta.tdAttr = 'data-qtip="Bloqué"';
-                        break;
-                }
-                return '';
-            },
-        },
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createIdCol(),
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createStateCol(),
         {
             name: 'network',
             editor: {
@@ -45,46 +21,10 @@ Ext.define('MyDesktop.modules.router.models.MyNetworkModel', {
             type: 'string',
             searchable: true
         },
-        {
-            name: 'comment',
-            editor: {
-                editable: true,
-                allowBlank: false
-            },
-            type: 'string'
-        },
-        {name: 'created_date', type: 'date', text: 'Crée le',
-            renderer: function (value, metaData, record, rowIndex, colIndex, store) {
-                var nameCreator = 'inconnu';
-                var idCreator = record.get('created_by');
-                var st = this.store;
-                var index = st.find('id', idCreator);
-                if (index >= 0)
-                    nameCreator = st.getAt(index).get('username');
-                if (value == null)
-                    value = 'Inconnu';
-                return 'le : <i>' + value + '</i><br>par: <i>' + nameCreator + '</i></br>';
-            },
-        },
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createCommentCol(),
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createCreatedCol(),
         {name: 'created_by', binded: false},
-        {name: 'modified_date', type: 'date', text: 'Modifié le',
-            renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
-                var nameModifier = 'inconnu';
-                var idModifier = record.get('modified_by');
-                var st = this.store;
-                var index = st.find('id', idModifier);
-                if (index >= 0)
-                    nameModifier = st.getAt(index).get('username');
-                if (record.data.modified_date === record.data.created_date)
-                {
-                    return 'non modifié';
-                }
-                else
-                {
-                    return 'le : <i>' + value + '</i><br>par: <i>' + idModifier + '</i></br>';
-                }
-            }
-        },
+        Ext.create('MyDesktop.modules.common.gridcols.gridCols').createModifiedCol(),
         {name: 'modified_by', binded: false},
     ]
 });
