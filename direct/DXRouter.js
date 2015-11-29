bd1bb0ea09d33fb8adb3b3e5887e9dec12178638
thierry <thierry@PC-THIERRY>
2015-11-29 19:29:17 +0100

    - prise en charge du parsing des logs du routeur

--- direct/DXRouter.js
+++ direct/DXRouter.js
@@ -4,9 +4,10 @@
  * 
  */
 var log = global.log.child({widget_type: 'DXRouter'});
+var fs = require('fs');
+var split = require('split');
 var pool = global.pool;
 var DXCommon = require('../tools/DXCommon.js');
-
 var DXRouter = {
     // method signature has 5 parameters
     /**
@@ -64,7 +65,7 @@ var DXRouter = {
         if (!params) {
             var params = [];
         }
-        var query='';
+        var query = '';
         var table = 'relay_domains';
         params.every(function (param) {
             // test erreur///
@@ -84,7 +85,7 @@ var DXRouter = {
         if (!params) {
             var params = [];
         }
-        var query='';
+        var query = '';
         var table = 'mynetworks';
         params.forEach(function (entry) {
             count++;
@@ -92,13 +93,71 @@ var DXRouter = {
             //if (count == 2)
             // entry.domain = 'aa' + entry.domain;
             query += "DELETE FROM " + table + " WHERE ";
-            query += "id="+param.id + " AND ";
-            query += "state='"+param.state + "' AND ";
-            query += "username='"+param.network + "' AND ";
-            query += "lastname='"+param.comment + "';";
+            query += "id=" + param.id + " AND ";
+            query += "state='" + param.state + "' AND ";
+            query += "username='" + param.network + "' AND ";
+            query += "lastname='" + param.comment + "';";
             return true;
         });
         DXCommon.destroy2(query, callback, sessionID, request, response, log);
+    },
+    getRouterLogsMailinfo: function (params, callback, sessionID, request, response) {
+        var data = [];
+        var totalCount = 0;
+        var position = -2; // peut être mis a -1 par line.search attention, donc -2
+        if (!params)
+            var params = {};
+        if (!params.col)
+            params.col = 'message';
+        if (!params.start)
+            params.start = 0;
+        if (!params.limit)
+            params.limit = 50;
+        params.log = log;
+        var stream = fs.createReadStream('/var/log/postfix/mail.info', {
+            flags: 'r',
+            encoding: 'utf-8',
+            fd: null,
+            mode: 0666,
+            bufferSize: 64 * 1024
+        });
+        stream.pipe(split())
+                .on('data', function (line) {
+                    //each chunk now is a seperate line!
+                    if (params.search)
+                        position = line.search(params.search);
+                    if (position === -2 || position > -1) {
+                        var tline = '';
+                        // on parse en premier les mails echangés
+                        var regex = /^(\w{1,3} \d{1,2} \d{1,2}:\d{1,2}:\d{1,2}) ([\w.-]+) (.+]): (.+): ([from|to].+)/;
+                        tline = line.match(regex);
+                        // console.log("line",line);
+                        if (tline !== null)
+                            totalCount++;
+                        if (totalCount >= params.start && totalCount <= params.start + params.limit) {
+                            if (tline !== null)
+                            {
+                                var itemData = {};
+                                itemData.id = totalCount;
+                                itemData.date = tline[1];
+                                itemData.host = tline[2];
+                                itemData.process = tline[3];
+                                itemData.key = tline[4];
+                                itemData.message = tline[5];
+                                data.push(itemData);
+                            }
+                        }
+                    }
+                })
+                .on('end', function () {
+                    console.log('fin du fichier', totalCount);
+                    var message = {
+                        'ZMTypeCode': 'DX',
+                        'ZMErrorCode': 300
+                    };
+                    stream.destroy();
+                    DXCommon.sendMsg(true, message, data, callback, totalCount);
+                });
     },
     getRelayDomains: function (params, callback, sessionID, request, response) {
         var query, extraQuery;
@@ -187,9 +246,4 @@ var DXRouter = {
         DXCommon.update(params, callback, sessionID, request, response);
     },
 };
-
-
-
-
-
 module.exports = DXRouter;
